05c5834d272678ebbafce879fd5815513676f0be
Dmytro Kholodov <surakamy@gmail.com>
2019-10-22 12:40:23 +0300

    Project 4

---
+++ P3_BetterRest/P3_BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P3_BetterRest
+//
+//  Created by Dmytro Kholodov on 10/21/19.
+//  Copyright © 2019 TheHackingWithSwift. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
