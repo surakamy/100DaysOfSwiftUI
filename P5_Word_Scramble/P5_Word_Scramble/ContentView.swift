43423bb7b9ce6bb49ff9c7ec0388f97d424cf81d
Dmytro Kholodov <surakamy@gmail.com>
2019-10-23 13:49:56 +0300

    Day 30

--- P5_Word_Scramble/P5_Word_Scramble/ContentView.swift
+++ P5_Word_Scramble/P5_Word_Scramble/ContentView.swift
@@ -52,36 +52,47 @@ struct WordView: View {
 }
 
 struct ContentView: View {
-    @State var allWords = ContentView.loadWords()
+    //@State var allWords: [String] = ContentView.loadWords()
 
-    @ObservedObject var search = SearchTextObservable()
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
 
-    @State var isSearching = false
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
 
     var body: some View {
         NavigationView {
             List() {
-                Section(header: EmptyView()) {
-                    Text(search.searchText)
-                    SearchField(searchTextWrapper: search, placeholder: "Any word?", isSearching: $isSearching)
+                Section(header: Text("A new word?")) {
+                    TextField("Enter here", text: $newWord, onCommit: addNewWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .autocapitalization(.none)
                 }
 
-                Section(header: EmptyView()) {
-                    ForEach(allWords) { w in
+                Section(header: Text("Words")) {
+                    ForEach(usedWords) { w in
                         HStack {
-                            Spacer()
+                            Image(systemName: "\(w.count).circle")
                             WordView(word: w)
                             Spacer()
                         }
                     }
                 }
-            }
+
+
+            }.navigationBarTitle(rootWord)
         }
 
-        .onAppear {
-            self.search.searchSubject.sink { s in
-                print(s)
-            }
+
+        .onAppear(perform: startGame)
+
+
+
+        .alert(isPresented: $showingError) {
+            Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
         }
     }
 
@@ -90,6 +101,71 @@ struct ContentView: View {
         let resUrl = Bundle.main.url(forResource: "start", withExtension: "txt")
         let rawString = try! String(contentsOf: resUrl!)
         return rawString.split(separator: "\n").map(String.init)
+    }
+
+    private func startGame() {
+        let allWords: [String] = ContentView.loadWords()
+        rootWord = allWords.randomElement() ?? "silkworm"
+    }
+
+    private func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    private func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    private func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 
 }
